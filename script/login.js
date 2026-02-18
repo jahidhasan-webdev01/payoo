1a65a28ca20e953122e7ac5135090a5dce666bf3
jahidhasan-webdev01 <jahidhasan.webdev01@gmail.com>
2026-02-19 00:27:37 +0600

    implement condition on login function

--- script/login.js
+++ script/login.js
@@ -1,1 +1,20 @@
-console.log("YOOO")
+document.getElementById("btn-login")
+    .addEventListener("click", function () {
+        const numberInput = document.getElementById("input-number").value;
+        const pinInput = document.getElementById("input-pin").value;
+
+        if (numberInput.length < 11) {
+            return alert("Invalid number")
+        }
+
+        if (pinInput.length < 4) {
+            return alert("Invalid PIN")
+        }
+
+        if (numberInput === "01774266484" && pinInput === "1234") {
+            alert("Login success");
+            window.location.assign("/home.html");
+        } else {
+            return alert("Login failed!");
+        }
+    }) 
