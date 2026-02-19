352ffa406c51f2d41a52cdb46c2034c72a8a8d97
jahidhasan-webdev01 <jahidhasan.webdev01@gmail.com>
2026-02-19 15:49:49 +0600

    implement history and design send money section

--- script/addMoney.js
+++ script/addMoney.js
@@ -21,6 +21,25 @@ getElementByID("btn-add-money")
             return alert("Invalid PIN");
         }
 
+        // Add in history
+        const historyEl = getElementByID("history-container");
+        const newHistory = document.createElement("div");
+        newHistory.innerHTML = 
+        ` <div class="history-card pl-8 pr-12 py-4 rounded-2xl bg-white flex justify-between border border-gray-200 hover:bg-primary hover:text-primary-content">
+                <div class="flex items-center gap-4">
+                    <div class="w-[60px] h-[60px] bg-gray-200 rounded-full overflow-hidden p-4">
+                        <img src="./assets/opt-1.png" alt="opt-1" class="w-full h-full object-cover">
+                    </div>
+                    <div>
+                        <h2 class="font-bold">Bank Deposit</h2>
+                        <p class="text-sm mt-1">${formatDateTime(new Date())}</p>
+                    </div>
+                </div>
+                <i class="fa-solid fa-ellipsis rotate-90"></i>
+            </div>
+        `
+
+        historyEl.append(newHistory)
         // If pass every test
         setBalance(newBalance);
         alert(`Money add to your account from ${inputBankName} at ${new Date()}.
--- script/functions.js
+++ script/functions.js
@@ -7,10 +7,58 @@ function getElementByID(_id) {
     return document.getElementById(_id);
 }
 
+function getElementByClassName(_className) {
+    return document.getElementsByClassName(_className);
+}
+
 function getCurrentBalance() {
     return Number(getElementByID("balance-count").innerText);
 }
 
-function setBalance (_newBalance) {
+function setBalance(_newBalance) {
     getElementByID("balance-count").innerText = _newBalance;
+}
+
+function showOnly(_id) {
+    // Hide all tags
+    const allTags = document.querySelectorAll(".tag");
+    allTags.forEach(tag => {
+        tag.classList.add("hidden");
+    })
+
+    // Remove hidden from selected element
+    getElementByID(_id).classList.remove("hidden");
+
+    // For showing active
+    const buttons = document.querySelectorAll(".tag-menu");
+    buttons.forEach(btn => {
+        btn.classList.remove("bg-primary", "text-primary-content");
+    })
+
+    // Get selected button
+    const selectedButton = getElementByClassName(`tag-${_id}`)[0];
+
+    selectedButton.classList.add("bg-primary", "text-primary-content");
+}
+
+function formatDateTime(date) {
+    const now = new Date();
+
+    const isToday =
+        date.getDate() === now.getDate() &&
+        date.getMonth() === now.getMonth() &&
+        date.getFullYear() === now.getFullYear();
+
+    // Format time
+    const time = date.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: true
+    });
+
+    if (isToday) {
+        return `Today ${time}`;
+    } else {
+        return date.toLocaleDateString() + " " + time;
+    }
 }
